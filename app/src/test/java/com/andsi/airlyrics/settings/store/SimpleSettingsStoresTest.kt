ba5ed9a3450a05edb470d55d6a73a36fae2dcbb9
bing <bingxyz1024@gmail.com>
2026-09-14 18:44:56 +0800

    feat(i18n): support Android app languages

--- app/src/test/java/com/andsi/airlyrics/settings/store/SimpleSettingsStoresTest.kt
+++ app/src/test/java/com/andsi/airlyrics/settings/store/SimpleSettingsStoresTest.kt
@@ -116,4 +116,28 @@ class SimpleSettingsStoresTest : SettingsStoreTestBase() {
         )
     }
 
+    @Test
+    fun languageSettingsStore_mapsSystemLanguageTagsToSupportedModes() {
+        assertEquals(
+            LanguageSettingsStore.MODE_SYSTEM,
+            LanguageSettingsStore.modeFromLanguageTags("")
+        )
+        assertEquals(
+            LanguageSettingsStore.MODE_EN,
+            LanguageSettingsStore.modeFromLanguageTags("en-US")
+        )
+        assertEquals(
+            LanguageSettingsStore.MODE_ZH_CN,
+            LanguageSettingsStore.modeFromLanguageTags("zh-Hans-CN")
+        )
+        assertEquals(
+            LanguageSettingsStore.MODE_ZH_TW,
+            LanguageSettingsStore.modeFromLanguageTags("zh-Hant-TW")
+        )
+        assertEquals(
+            LanguageSettingsStore.MODE_ZH_TW,
+            LanguageSettingsStore.modeFromLanguageTags("zh-HK")
+        )
+    }
+
 }
