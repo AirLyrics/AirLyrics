726240b9d5649a1aaa5df044aa1291c050f93a95
AndSi-327 <forzesenasri@gmail.com>
2026-09-22 18:35:47 +0800

    prepare v1.2.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -91,8 +91,8 @@ android {
         minSdk = 26
         //noinspection OldTargetApi
         targetSdk = 36
-        versionCode = 16
-        versionName = "1.2.5"
+        versionCode = 17
+        versionName = "1.2.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
         ndk {
